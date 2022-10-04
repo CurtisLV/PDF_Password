35705140c532fb61826fde997164077cdf308d04
CurtisLV <curtislv@gmail.com>
2022-10-04 19:20:09 +0300

    19:20

--- Program.cs
+++ Program.cs
@@ -15,13 +15,17 @@ static void fw_Created(object sender, FileSystemEventArgs e)
 {
     System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
     string path = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\documents";
-    PdfDocument document = PdfReader.Open(path + "\\" + e.Name, "");
-
-    PdfSecuritySettings securitySettings = document.SecuritySettings;
-
-    // Setting one of the passwords automatically sets the security level to 
-    // PdfDocumentSecurityLevel.Encrypted128Bit.
-    securitySettings.UserPassword = "abc123";
+    using (PdfDocument document = PdfReader.Open(path + "\\" + e.Name, ""))
+    {
+         PdfSecuritySettings securitySettings = document.SecuritySettings;
+        // Setting one of the passwords automatically sets the security level to 
+        // PdfDocumentSecurityLevel.Encrypted128Bit.
+        securitySettings.UserPassword = "abc123";
+        // Save the document...
+        document.Save(path + "\\" + e.Name);
+        Console.WriteLine($"Password applied: {e.Name}");
+    }
+    
     //securitySettings.OwnerPassword = "abc123";
 
     // Don't use 40 bit encryption unless needed for compatibility reasons
@@ -37,8 +41,7 @@ static void fw_Created(object sender, FileSystemEventArgs e)
     //securitySettings.PermitModifyDocument = false;
     //securitySettings.PermitPrint = true;
 
-    // Save the document...
-    document.Save(path + "\\" + e.Name);
+
 }
 
 
